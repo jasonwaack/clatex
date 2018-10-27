8459f24a344914c97ca016263b1b9d753dd8bbf8
jasonwaack <jasonwaack@gmail.com>
2018-10-27 12:50:22 -0400

    first commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clatex
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clatex/core_test.clj
@@ -0,0 +1,7 @@
+(ns clatex.core-test
+  (:require [clojure.test :refer :all]
+            [clatex.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
